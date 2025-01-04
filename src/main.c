69c26f28e5d7b65af14a1160b9cbe96bf4170fdd
Maxwell D. Dorliea <deboy170@gmail.com>
2025-01-04 22:58:11 +0000

    Handled Number literals

--- src/main.c
+++ src/main.c
@@ -69,7 +69,7 @@ int scanNum(char *s, int start, int end, int line, int* hasError) {
   while (start < end) {
     char c = s[start];
     putc(c, stdout);
-    if (c == ' ' || c == '\n') {
+    if (c == ' ' || c == '\n' || start == end - 1) {
       str[i] = '\0';
       if (!deci) deci = 1;
       printf("NUMBER %s %.*f\n", str, deci, strtod(str, &derr));
